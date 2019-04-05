b6b8bf2e8296c4da5dbeae5cef4fb25c83cc62c1
kk <1025023440@qq.com>
2019-04-06 00:08:52 +0800

    1

--- server/Request.php
+++ server/Request.php
@@ -1,6 +1,8 @@
 <?php
 namespace server;
 
+use Response;
+
 class Request
 {
 	public static $request;
@@ -8,11 +10,21 @@ class Request
 	public static function init($request){
 		self::$request = $request;
 		$pathinfo = self::$request->server['path_info'];
+
+		$className = self::getClassName($pathinfo);
+		
+	}
+
+	public static function getClassName($pathinfo){
 		$pathStr = trim($pathinfo,'/');
 		$pathArr = explode('/', $pathStr);
 
+		if(empty($pathArr) || count($pathArr) != 3){
+			Response::send('调用错误，请使用pathinfo路径调用');
+		}
+
 		$className = $pathArr[0] . '\\' . ucfirst($pathArr[1]) . '\\' . $pathArr[2];
-		
+
 		print_r($pathStr);
 		print_r($pathArr);
 		print_r($className);
