fdb2f76ab988e60e338985492ff3057da898e643
kk <1025023440@qq.com>
2019-04-05 23:51:37 +0800

    1

--- server/Request.php
+++ server/Request.php
@@ -8,7 +8,9 @@ class Request
 	public static function init($request){
 		self::$request = $request;
 		$pathinfo = self::$request->server['path_info'];
+		$pathStr = trim($pathinfo,'/');
 		$pathArr = explode('/', $pathinfo);
+		print_r($pathStr);
 		print_r($pathArr);
 	}
 }
