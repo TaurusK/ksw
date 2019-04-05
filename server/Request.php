18399ec94170e372eb95567dec5368ecb68642b5
kk <1025023440@qq.com>
2019-04-06 00:00:39 +0800

    1

--- server/Request.php
+++ server/Request.php
@@ -10,7 +10,11 @@ class Request
 		$pathinfo = self::$request->server['path_info'];
 		$pathStr = trim($pathinfo,'/');
 		$pathArr = explode('/', $pathStr);
+
+		$className = $pathArr[0] . '\\' . ucfirst($pathArr[1]) . '\\' . $pathArr[2];
+		
 		print_r($pathStr);
 		print_r($pathArr);
+		print_r($className);
 	}
 }
