f9656a86b4ee2c0f63c1864b0b28b6873eaf742e
kk <1025023440@qq.com>
2019-04-06 14:21:07 +0800

    1

--- server/base.php
+++ server/base.php
@@ -6,8 +6,12 @@ define('SERVER_PATH', ROOT_PATH . 'server/');
 //扩展目录
 define('EXTEND_PATH', ROOT_PATH . 'extend/');
 
+//服务扩展目录
+define('EXT_PATH', SERVER_PATH . 'ext/');
+
 
 require_once SERVER_PATH . 'Loader.php';
-require_once SERVER_PATH . 'Task.php';
 require_once SERVER_PATH . 'Request.php';
 require_once SERVER_PATH . 'Response.php';
+
+require_once EXT_PATH . 'Task.php';
